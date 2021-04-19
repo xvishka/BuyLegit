52678254f7dbd5234f87b161c2a1d41bf886ac05
Nimna <ravishanifer@gmail.com>
2021-04-19 20:14:54 +0530

    Add database queries to insert data

--- app.py
+++ app.py
@@ -1,95 +1,112 @@
-#from flask import Flask
-#app = Flask(__name__)
-#@app.route("/")
-#def home():
-#    return "Hello Nimna ! This is your first flask app."
-
 import flask
 from flask import request, jsonify
 import json
 import csv
 from flask_cors import CORS
-
+import pymysql
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
 
-productList=[]
-originalProductList_1=[]
-originalProductList_2=[]
+# Conneting Database
+database = pymysql.connect(
+    host='apicem-db.cud06hsz15em.us-east-2.rds.amazonaws.com',
+    user='admin',
+    port = 3306,
+    password = "admin123",
+    db='buylegit',
+    )
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>BuyLegit data set !</p>"
+# ALGO : Inserting it to the database for Category.
+def insertingCategory(cateId,Category):
+    cursor = database.cursor()
+    cursor.execute("""INSERT INTO Category (CategoryId ,Category )
+                    VALUES (%s,%s);""",(cateId,Category))
+    database.commit()
 
-f = open('E:\IIT_Second_Year\SDGP\ApicemData.json',)
+# ALGO : getting data from the list and inserting it to the database for Unique Products.
+def insertingUniqueProducts(itemId,rank,title,categoryId,url,selName,feedS,posFeed,topRate,price,img,hasDupli):
+    cursor = database.cursor()
+    cursor.execute("""INSERT INTO UniqueProducts (ItemId,ProductRank ,Title,CategoryId,
+                    Url,SellerUserName,FeedbackScore,
+                    PostiveFeedbackPercent,TopRatedSeller,Price,Image,HasDuplicate)
+                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s );""",(itemId,rank,title,categoryId,url,selName,feedS,posFeed,topRate,price,img,hasDupli))
+    database.commit()
 
-data = json.load(f)
+# ALGO : getting data from the list and inserting it to the database for Duplicate Products.
+def insertingDuplicateProducts(itemId,title,categoryId,url,selName,feedS,posFeed,topRate,price,img,masterId):
+    cursor = database.cursor()
+    cursor.execute("""INSERT INTO DuplicateProducts (ItemId,Title,CategoryId,
+                    Url,SellerUserName,FeedbackScore,
+                    PostiveFeedbackPercent,TopRatedSeller,Price,Image,masterId)
+                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s );""",(itemId,title,categoryId,url,selName,feedS,posFeed,topRate,price,img,masterId))
+    database.commit()
 
-for i in data['products']:
-    productList.append(i)
-f.close()
+# all product list from the json
+productList = []
+similarityList = []
 
-# Get copies of loaded product list
-originalProductList_1 = productList.copy()
-originalProductList_2 = productList.copy()
+try:
+    f = open('ApicemData.json',)
+    data = json.load(f)
 
-# Return all original product list
-@app.route('/api/product/all', methods=['GET'])
-def api_all():
-    return jsonify(originalProductList_1)
+    for q in data['products']:
+        productList.append(q)
 
-# Send navigate page product list sort 
-@app.route('/api/product/navigatePage/original', methods=['GET'])
-def api_navigate_page_original():
-    if 'startNumber' in request.args:
-        startNumber = request.args['startNumber']
-    else:
-        return "No start number provided. Please specify a start number"
-    
-    if 'endNumber' in request.args:
-        endNumber = request.args['endNumber']
-    else:
-        return "No end number provided. Please specify a end number"
-    
-    navigatePageProduct = []
-    
-    startIndex = int(startNumber)
-    endIndex = int(endNumber)
+    f.close()
 
-    if (startIndex < len(originalProductList_1) and (endIndex < len(originalProductList_1))):
-        navigatePageProduct = originalProductList_1[startIndex-1:endIndex]
+except:
+    print("'ApicemData.json' File not Found")
+
+try:
+    # Load Unique data and duplicate dat separately
+    ml = open('similarity.json',)
+    mlData = json.load(ml)
+
+    for k in mlData:
+        similarityList.append(k)
+    ml.close()
+except:
+    print("'similarity.json' File not Found")
+
+listItems = []
+listItems2 = []
+
+if(len(similarityList) != 0):
+    for r in similarityList:
+        master = r['master_pi']
+        if master not in listItems:
+            items = []
+            listItems.append(master)
+            items.append(master)
+            for l in similarityList:
+                if(master == l['similar_pi']):
+                    listItems.append(l['master_pi'])
+                    items.append(l['master_pi'])
+            listItems2.append(items)  
         
-    return jsonify(navigatePageProduct)
+print(listItems2)
 
-# return selected item according to requested item ID
-@app.route('/api/product', methods=['GET'])
-def api_id():
+# get product category details
+if(len(productList) != 0):
+    findCategoryProduct = productList[0]
+    findCategoryId = findCategoryProduct['categoryId']
+    findCategoryName = findCategoryProduct['category']
 
-    if 'itemId' in request.args:
-        itemId = request.args['itemId']
-        print(itemId)
+# INSERT CATEGORY DETAILS TO CATEGORY TABLE
+try:
+    insertingCategory(findCategoryId, findCategoryName)
+except:
+    print("This category: " , findCategoryName, " already exists!")
 
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    selectedProduct = []
-
-    for product in originalProductList_1:
-        print("product itemId: ", product['itemId'])
+# before adding duplicated recommended products
+print("Length before adding duplicated Recommended Products ", len(productList))
        
-        if product['itemId'] == itemId:
-            selectedProduct.append(product)
-    print("selected product list: " , selectedProduct)
-
-    return jsonify(selectedProduct)
-
 # sort product list according to the product price
 def sortPrice(dataList:list):
     listLength = len(dataList)
     
-
     if listLength != 0:
         for i in range(listLength-1):
             for j in range(0,listLength-i-1):
@@ -143,10 +160,8 @@ def sortPositiveFeedbackPercentage(dataList:list):
 
                 if(float(feedbackScorePercent_j[0]) < float(feedbackScorePercent_k[0])):
                     dataList[j],dataList[j+1] = dataList[j+1],dataList[j]
-                      
 
 #sort final output lis according to the top rated seller
-
 def sortTopRatedSellers(dataList:list):
     listLength = len(dataList)
 
@@ -165,167 +180,189 @@ def sortTopRatedSellers(dataList:list):
                 if(topRatedValue_j[0] == "false" ) and (topRatedValue_k[0] == "true"):
                     dataList[j],dataList[j+1] = dataList[j+1],dataList[j]
 
+def sortFinalOutput(dataList:list):
 
-#Sorted recommended products                  
-recomendProducts = []  
+    originalProductList_2 = dataList.copy()
+    length = len(dataList)
 
-sortPrice(productList)
+    recomendProducts = []
+    sortPrice(dataList)
 
-# Return all products sort according to the price
-@app.route('/api/product/all/priceSort', methods=['GET'])
-def api_all_priceSort():
-    return jsonify(productList)
+    recomendProducts.append(dataList[0])
 
+    sortPositiveFeedbackScore(originalProductList_2)
+    recomendProducts.append(originalProductList_2[0])
 
-# Send navigate page product list sort according to product price
-@app.route('/api/product/navigatePage/priceSort', methods=['GET'])
-def api_navigate_page_priceSort():
-    if 'startNumber' in request.args:
-        startNumber = request.args['startNumber']
-    else:
-        return "No start number provided. Please specify a start number"
-    
-    if 'endNumber' in request.args:
-        endNumber = request.args['endNumber']
-    else:
-        return "No end number provided. Please specify a end number"
-    
-    navigatePageProduct = []
-    
-    startIndex = int(startNumber)
-    endIndex = int(endNumber)
+    totalPrice = 0
+    price_j = 0
 
-    if (startIndex < len(productList) and (endIndex < len(productList))):
-        navigatePageProduct = productList[startIndex-1:endIndex]
-        
-    return jsonify(navigatePageProduct)
+    for j in range(length):
+        product_j = dataList[j]
+        price_j =product_j["price"]
+        totalPrice = totalPrice + float(price_j) 
 
+    # Find product price average
+    priceAverage = totalPrice / len(dataList)
 
-recomendProducts.append(productList[0])
+    priceRecommendedProduct = recomendProducts[0]
+    price = priceRecommendedProduct["price"]
 
-sortPositiveFeedbackScore(originalProductList_2)
+    priceAveragePercentage = ((priceAverage - float(price)) / priceAverage) * 100
 
-# Return all products sort according to the feedback score
-@app.route('/api/product/all/feedbackScoreSort', methods=['GET'])
-def api_all_feedbackScoreSort():
-    return jsonify(originalProductList_2)
+    totalFeedbackScore = 0
+    feedBackScoreIntValue = 0
+    feedbackScoreList = [] 
 
+    for j in range(length):
+        product_j = originalProductList_2[j]
+        seller_j = product_j["seller"]
+        feedbackScoreList = seller_j["feedbackScore"]
+        feedBackScoreIntValue = feedbackScoreList[0]
+        totalFeedbackScore = totalFeedbackScore + int(feedBackScoreIntValue)
 
-# Send navigate page product list sort according to product feedbackscore
-@app.route('/api/product/navigatePage/feedbackScoreSort', methods=['GET'])
-def api_navigate_page_feedbackSore():
-    if 'startNumber' in request.args:
-        startNumber = request.args['startNumber']
-    else:
-        return "No start number provided. Please specify a start number"
-    
-    if 'endNumber' in request.args:
-        endNumber = request.args['endNumber']
-    else:
-        return "No end number provided. Please specify a end number"
-    
-    navigatePageProduct = []
-    
-    startIndex = int(startNumber)
-    endIndex = int(endNumber)
+    # Find product price average
+    feedbackAverage = totalFeedbackScore / len(dataList)
 
-    if (startIndex < len(originalProductList_2) and (endIndex < len(originalProductList_2))):
-        navigatePageProduct = originalProductList_2[startIndex-1:endIndex]
-        
-    return jsonify(navigatePageProduct)
+    recommendedFeedbackScoreProduct = recomendProducts[1]
 
-    
-recomendProducts.append(originalProductList_2[0])
-print(recomendProducts)
-print(len(recomendProducts))
+    seller = recommendedFeedbackScoreProduct["seller"]
+    feedbackScore = seller["feedbackScore"]
 
-length = len(productList)
+    feedbackScoreAveragePercentage = ((feedbackAverage - float(feedbackScore[0])) / feedbackAverage) * 100
 
-totalPrice = 0
-price_j = 0
-for j in range(length):
-    product_j = productList[j]
-    price_j =product_j["price"]
-    totalPrice = totalPrice + float(price_j) 
+    finalOutput = []
 
-print("Total Price : ")
-print(totalPrice)
-# Find product price average
-priceAverage = totalPrice / len(productList)
-print("Price average  : ")
-print(priceAverage)
-
-priceRecommendedProduct = recomendProducts[0]
-price =priceRecommendedProduct["price"]
-
-priceAveragePercentage = ((priceAverage - float(price)) / priceAverage) * 100
-print("Price percentage : ")
-print(priceAveragePercentage)
-
-totalFeedbackScore = 0
-feedBackScoreIntValue = 0
-feedbackScoreList = [] 
-
-for j in range(length):
-    product_j = originalProductList_2[j]
-    seller_j = product_j["seller"]
-    feedbackScoreList = seller_j["feedbackScore"]
-    feedBackScoreIntValue = feedbackScoreList[0]
-    totalFeedbackScore = totalFeedbackScore + int(feedBackScoreIntValue)
-
-print("Total feedback score : ")
-print(totalFeedbackScore)
-# Find product price average
-feedbackAverage = totalFeedbackScore / len(productList)
-print("Feedback Score average  : ")
-print(feedbackAverage)
-
-recommendedFeedbackScoreProduct = recomendProducts[1]
-
-seller = recommendedFeedbackScoreProduct["seller"]
-feedbackScore = seller["feedbackScore"]
-
-feedbackScoreAveragePercentage = ((feedbackAverage - float(feedbackScore[0])) / feedbackAverage) * 100
-print("Feedback Score percentage : ")
-print(feedbackScoreAveragePercentage)
-
-# Find final recommended product list
-finalOutput =[]
-
-if(priceAveragePercentage > feedbackScoreAveragePercentage):
-    finalOutput = productList.copy() 
-elif(priceAveragePercentage < feedbackScoreAveragePercentage):
-    finalOutput = originalProductList_2.copy()
-else:
-    product_1 = recomendProducts[0]
-    product_1_seller = product_1["seller"]
-    feedbackPrecentage_1 = product_1_seller["positiveFeedbackPercent"]
-    precentage_1_IntValue = float(feedbackPrecentage_1[0])
-
-    product_2 = recomendProducts[1]
-    product_2_seller = product_2["seller"]
-    feedbackPrecentage_2 = product_2_seller["positiveFeedbackPercent"]
-    precentage_2_IntValue = float(feedbackPrecentage_2[0])
-    if(precentage_1_IntValue < precentage_2_IntValue):
+    if(priceAveragePercentage > feedbackScoreAveragePercentage):
+        finalOutput = dataList.copy() 
+    elif(priceAveragePercentage < feedbackScoreAveragePercentage):
         finalOutput = originalProductList_2.copy()
     else:
-        finalOutput = productList.copy() 
+        product_1 = recomendProducts[0]
+        product_1_seller = product_1["seller"]
+        feedbackPrecentage_1 = product_1_seller["positiveFeedbackPercent"]
+        precentage_1_IntValue = float(feedbackPrecentage_1[0])
 
-print(finalOutput)
-# sort final output lis according to the top rated seller
-sortTopRatedSellers(finalOutput)
+        product_2 = recomendProducts[1]
+        product_2_seller = product_2["seller"]
+        feedbackPrecentage_2 = product_2_seller["positiveFeedbackPercent"]
+        precentage_2_IntValue = float(feedbackPrecentage_2[0])
+        if(precentage_1_IntValue < precentage_2_IntValue):
+            finalOutput = originalProductList_2.copy()
+        else:
+            finalOutput = dataList.copy() 
 
-print(finalOutput)
+    # sort final output lis according to the top rated seller
+    sortTopRatedSellers(finalOutput)
 
+    return finalOutput
 
-# Final output
-@app.route('/api/product/all/finalProductList', methods=['GET'])
-def api_all_finalProductList():
+similarProductList = []
 
-    return jsonify(finalOutput)
+if(len(listItems2) != 0):
+    for i in listItems2:
+        duplicateList = i
+        duplicateProductList = []
 
-    
-print(len(productList))
+        for j in duplicateList:
+            duplicateItem = j
+
+            for k in productList:    
+                idNumber = k["itemId"]
+            
+                if (idNumber == duplicateItem):
+                    getProduct = k
+                    duplicateProductList.append(getProduct)
+                    productList.remove(getProduct)
+                
+        #sort duplicate product list
+        tempList = []
+        tempList = sortFinalOutput(duplicateProductList)
+
+        masterId = tempList[0]
+
+        for a in tempList:
+            if(masterId != a):
+                a["isDuplicate"] = masterId["itemId"]
+                similarProductList.append(a)
+            else:
+                # print("master: " , a["itemId"])
+                a["isDuplicate"] = "true"
+
+        # print(tempList[0])
+        productList.append(tempList[0])
+
+# after adding duplicated recommended products
+print("Length after adding duplicated Recommended Products ", len(productList)) 
+
+recommendedUniqueProductList = []
+recommendedUniqueProductList = sortFinalOutput(productList)
+
+# define the product rank
+for c in range(len(recommendedUniqueProductList)):
+    product = recommendedUniqueProductList[c]      
+    # Recommended product Rank start with 0
+    product['productRank'] = c
+
+#convert categoryId to int
+categoryIdForDB = int(findCategoryId)
+
+# WRITE UNIQUE RECOMMENDED PRODUCTS TO THE DATABASE
+if(len(recommendedUniqueProductList) != 0):
+    for x in recommendedUniqueProductList:
+        uniqueProduct = x
+        uniqueProductId = uniqueProduct['itemId']
+        uniqueProductRank = uniqueProduct['productRank']
+        uniqueProductTitle = uniqueProduct['title']
+        uniqueProductUrl = uniqueProduct['url']
+
+        uniqueProductSeller = uniqueProduct['seller']
+
+        uniqueProductSellerUserName = uniqueProductSeller['sellerUserName'][0]
+        uniqueProductFeedbackScore = uniqueProductSeller['feedbackScore'][0]
+        uniqueProductPostiveFeedbackPercent = uniqueProductSeller['positiveFeedbackPercent'][0]
+        uniqueProductTopRatedSeller = uniqueProductSeller['topRatedSeller'][0]
+
+        uniqueProductPrice = uniqueProduct['price']
+        uniqueProductImage = uniqueProduct['image']
+        uniqueProductDuplicate = uniqueProduct['isDuplicate']
+
+        # print(uniqueProductId)
+
+        try:
+            insertingUniqueProducts(uniqueProductId, uniqueProductRank, uniqueProductTitle, categoryIdForDB, uniqueProductUrl, 
+                            uniqueProductSellerUserName, uniqueProductFeedbackScore, uniqueProductPostiveFeedbackPercent, uniqueProductTopRatedSeller,
+                            uniqueProductPrice, uniqueProductImage, uniqueProductDuplicate)
+        except:
+            print("No more products to add to Unique product table!")
+            break
+
+# WRITE THE DUPLICATED NON-RECOMMENDED PRODUCTS TO THE DATABASE
+if(len(similarProductList) != 0):
+    for s in similarProductList:
+        similarProduct = s
+        similarProductId = similarProduct['itemId']
+        similarProductTitle = similarProduct['title']
+        similarProductUrl = similarProduct['url']
+
+        similarProductSeller = similarProduct['seller']
+
+        similarProductSellerUserName = similarProductSeller['sellerUserName'][0]
+        similarProductFeedbackScore = similarProductSeller['feedbackScore'][0]
+        similarProductPostiveFeedbackPercent = similarProductSeller['positiveFeedbackPercent'][0]
+        similarProductTopRatedSeller = similarProductSeller['topRatedSeller'][0]
+
+        similarProductPrice = similarProduct['price']
+        similarProductImage = similarProduct['image']
+        similarProductDuplicate = similarProduct['isDuplicate']
+        # print(similarProduct)
+
+        try:
+            insertingDuplicateProducts(similarProductId, similarProductTitle, categoryIdForDB, similarProductUrl, similarProductSellerUserName,
+                                        similarProductFeedbackScore, similarProductPostiveFeedbackPercent, similarProductTopRatedSeller, 
+                                        similarProductPrice, similarProductImage, similarProductDuplicate)
+        except:
+            print("No more products to add to duplicate Products table!")
+            break
 
 app.run()
 
