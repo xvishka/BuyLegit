e550fbc53aa01d5b545c8e172071960c19078b59
thirandev <78394671+thirandev@users.noreply.github.com>
2021-04-21 01:09:26 +0530

    Add files via upload
    Display the relevant products and its duplicates, after clicking a certain product.

---
+++ src/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer class="bg-dark text-center text-lg-start">
+            <a class="navbar-brand">Developed by Apicem</a>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
